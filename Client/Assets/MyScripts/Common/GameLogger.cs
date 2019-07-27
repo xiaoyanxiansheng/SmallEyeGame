712577ccc5fb8cf5a81fafdd010147b3080852bd
xiaoyaoxiansheng <360507245@qq.com>
2019-07-27 17:17:56 +0800

    fix

---
+++ Client/Assets/MyScripts/Common/GameLogger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogger {
+
+
+    public static void LogError(string s)
+    {
+        Debug.LogError(s);
+    }
+    public static void LogInfo(string s)
+    {
+        Debug.Log(s);
+    }
+    public static void LogWarning(string s)
+    {
+        Debug.LogWarning(s);
+    }
+
+}
---
+++ Client/Assets/MyScripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Console.WriteLine("aaaaaaaaaaaa");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Console.WriteLine("aaaaaaaaaaaa " + Time.deltaTime);
+        Console.ReadLine();
+    }
+}
